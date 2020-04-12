5c674b622aa77c37cf7b4130eb885aafd039dffe
lounis <lounis@teeps.co>
2020-04-12 11:14:42 +0200

    taskbox UI

---
+++ storybook-static/precache-manifest.f698cfd6e22a7cc38918b90aed6e0c97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3462108b9ab432bb701e21a0df74a57c",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.6e84374de91a49ab3740.bundle.js"
+  },
+  {
+    "url": "runtime~main.6e84374de91a49ab3740.bundle.js"
+  },
+  {
+    "revision": "2c1a64a51a4065d7e081",
+    "url": "static/css/main.abe17246.chunk.css"
+  },
+  {
+    "url": "vendors~main.6e84374de91a49ab3740.bundle.js"
+  },
+  {
+    "url": "vendors~main.6e84374de91a49ab3740.bundle.js.LICENSE.txt"
+  }
+]);
